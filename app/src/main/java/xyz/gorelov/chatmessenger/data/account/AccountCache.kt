0f0d33abe958180704b1213a59d87773289b5a99
PurpleWaterInTheBottle <p.m.gorelov@gmail.com>
2021-10-22 19:12:11 +0300

    Data layer. Added features: Register. Update token

---
+++ app/src/main/java/xyz/gorelov/chatmessenger/data/account/AccountCache.kt
@@ -0,0 +1,10 @@
+package xyz.gorelov.chatmessenger.data.account
+
+import xyz.gorelov.chatmessenger.domain.type.Either
+import xyz.gorelov.chatmessenger.domain.type.None
+import xyz.gorelov.chatmessenger.domain.type.exception.Failure
+
+interface AccountCache {
+    fun getToken(): Either<Failure, String>
+    fun saveToken(token: String): Either<Failure, None>
+}
---
+++ app/src/main/java/xyz/gorelov/chatmessenger/data/account/AccountRemote.kt
@@ -0,0 +1,15 @@
+package xyz.gorelov.chatmessenger.data.account
+
+import xyz.gorelov.chatmessenger.domain.type.Either
+import xyz.gorelov.chatmessenger.domain.type.None
+import xyz.gorelov.chatmessenger.domain.type.exception.Failure
+
+interface AccountRemote {
+    fun register(
+        email: String,
+        name: String,
+        password: String,
+        token: String,
+        userDate: Long
+    ): Either<Failure, None>
+}
