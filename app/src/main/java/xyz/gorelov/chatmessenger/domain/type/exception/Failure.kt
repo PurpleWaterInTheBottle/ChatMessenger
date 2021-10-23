a7a0db0c71d6ff543e056faee45cddac45aa9dae
PurpleWaterInTheBottle <p.m.gorelov@gmail.com>
2021-10-23 17:13:32 +0300

    Domain layer. Added: EmailAlreadyExistError, HandleOnce

--- app/src/main/java/xyz/gorelov/chatmessenger/domain/type/exception/Failure.kt
+++ app/src/main/java/xyz/gorelov/chatmessenger/domain/type/exception/Failure.kt
@@ -7,4 +7,6 @@ package xyz.gorelov.chatmessenger.domain.type.exception
 sealed class Failure {
     object NetworkConnectionError : Failure()
     object ServerError : Failure()
+
+    object EmailAlreadyExistError : Failure()
 }
