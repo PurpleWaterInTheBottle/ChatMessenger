774400310aac84e8c9e44795d6afa76e47572687
PurpleWaterInTheBottle <p.m.gorelov@gmail.com>
2021-10-24 19:55:05 +0300

    Cache layer. Added: logout, get account, save account

--- app/src/main/java/xyz/gorelov/chatmessenger/cache/AccountCacheImpl.kt
+++ app/src/main/java/xyz/gorelov/chatmessenger/cache/AccountCacheImpl.kt
@@ -1,6 +1,7 @@
 package xyz.gorelov.chatmessenger.cache
 
 import xyz.gorelov.chatmessenger.data.account.AccountCache
+import xyz.gorelov.chatmessenger.domain.account.AccountEntity
 import xyz.gorelov.chatmessenger.domain.type.Either
 import xyz.gorelov.chatmessenger.domain.type.None
 import xyz.gorelov.chatmessenger.domain.type.Failure
@@ -15,5 +16,17 @@ class AccountCacheImpl @Inject constructor(private val prefsManager: SharedPrefs
 
     override fun getToken(): Either<Failure, String> {
         return prefsManager.getToken()
+    }
+
+    override fun logout(): Either<Failure, None> {
+        return prefsManager.removeAccount()
+    }
+
+    override fun getCurrentAccount(): Either<Failure, AccountEntity> {
+        return prefsManager.getAccount()
+    }
+
+    override fun saveAccount(account: AccountEntity): Either<Failure, None> {
+        return prefsManager.saveAccount(account)
     }
 }
--- app/src/main/java/xyz/gorelov/chatmessenger/domain/account/AccountEntity.kt
+++ app/src/main/java/xyz/gorelov/chatmessenger/domain/account/AccountEntity.kt
@@ -5,12 +5,12 @@ import com.google.gson.annotations.SerializedName
 class AccountEntity(
     @SerializedName("user_id")
     val id: Long,
-    val name: String,
-    val email: String,
+    val name: String?,
+    val email: String?,
     @SerializedName("token")
-    val token: String,
-    val status: String,
+    val token: String?,
+    val status: String?,
     @SerializedName("user_date")
     val userDate: Long,
-    val image: String
+    val image: String?
 )
