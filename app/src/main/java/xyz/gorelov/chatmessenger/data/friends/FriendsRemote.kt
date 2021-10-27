06118fa8574dd1f7c1969ed8f45ada96c56410ab
PurpleWaterInTheBottle <p.m.gorelov@gmail.com>
2021-10-27 19:36:19 +0300

    Data layer. Added: friends

---
+++ app/src/main/java/xyz/gorelov/chatmessenger/data/friends/FriendsRemote.kt
@@ -0,0 +1,17 @@
+package xyz.gorelov.chatmessenger.data.friends
+
+import xyz.gorelov.chatmessenger.domain.friends.FriendEntity
+import xyz.gorelov.chatmessenger.domain.type.Either
+import xyz.gorelov.chatmessenger.domain.type.Failure
+import xyz.gorelov.chatmessenger.domain.type.None
+
+interface FriendsRemote {
+    fun getFriends(userId: Long, token: String): Either<Failure, List<FriendEntity>>
+    fun getFriendRequests(userId: Long, token: String): Either<Failure, List<FriendEntity>>
+
+    fun approveFriendRequest(userId: Long, requestUserId: Long, friendsId: Long, token: String): Either<Failure, None>
+    fun cancelFriendRequest(userId: Long, requestUserId: Long, friendsId: Long, token: String): Either<Failure, None>
+
+    fun addFriend(email: String, userId: Long, token: String): Either<Failure, None>
+    fun deleteFriend(userId: Long, requestUserId: Long, friendsId: Long, token: String): Either<Failure, None>
+}
